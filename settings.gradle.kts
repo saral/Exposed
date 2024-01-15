f7da940e92710dea4143018f8c65528e92238ea8
Sinan Saral <sinansaral@gmail.com>
2024-01-15 16:14:20 +0100

    rename exposed-java-time to saral-java-time

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
-rootProject.name = "exposed"
+rootProject.name = "exposed-saral"
 include("exposed-core")
 include("exposed-dao")
 include("exposed-jodatime")
-include("exposed-java-time")
+include("saral-java-time")
 include("spring-transaction")
 include("exposed-spring-boot-starter")
 include("exposed-jdbc")
